22b542b34c3aa57758b31be6331781e51d2e32b7
Çağkan Özçivit <ozcivitcagkan@gmail.com>
2026-08-12 02:03:46 +0300

    remove unnecessary files

--- PYTHON/araclar.py
+++
@@ -1,5 +0,0 @@
-def kare_al(a):
-    return a * a
-
-def kup_al(b):
-    return b*b*b
--- PYTHON/class2_araba.py
+++ PYTHON/class2_araba.py
@@ -13,13 +13,13 @@ class Araba:
 
 
 araba1 = Araba("Toyota", "Corolla")
-araba1.bilgi_goster()      # Marka : Toyota Model : Corolla Hız : 0
+araba1.bilgi_goster()     
 
 araba1.hizlan(50)
-araba1.bilgi_goster()      # Marka : Toyota Model : Corolla Hız : 50
+araba1.bilgi_goster()   
 
 araba1.hizlan(30)
-araba1.bilgi_goster()      # Marka : Toyota Model : Corolla Hız : 80
+araba1.bilgi_goster()     
 
 araba2 = Araba("Citroen", "C4")
 araba2.bilgi_goster()
